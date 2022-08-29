86baa423d41ac84b3bfb071a490872a97e671d7c
Shridhar s <Shridharsirpee15@gmail.com>
2022-08-29 12:47:44 +0530

    new one

---
+++ src/test/java/CreateAccountTest/DummyTest.java
@@ -0,0 +1,16 @@
+package CreateAccountTest;
+
+import org.apache.xmlbeans.impl.xb.xsdschema.Public;
+import org.testng.annotations.Test;
+
+
+
+
+public class DummyTest  {
+
+
+	@Test
+	public void AdmissionFormPageTest() throws Throwable {
+		System.out.println("Login is done");
+	}
+}
